59aba2f191437906ada66b35ec729c618fb3a5dc
Elivan-Jr <elivanlimajunior@gmail.com>
2022-05-20 20:40:35 -0300

    Finalizando Exercício 1

---
+++ aula 12/exercico 1/script.js
@@ -0,0 +1,24 @@
+function carregar () {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('foto')
+    var msghr = document.getElementById('msghr')
+    var data = new Date()
+    var hora = //data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    msghr.innerHTML = `Bem vindo!`
+
+    if (hora >= 0 && hora < 12) {
+        img.innerHTML = '<img src="manha.png">'
+        document.body.style.background = '#FDE7AF'
+        msghr.innerText = `Bom dia!`
+
+    } else if (hora >= 12 && hora <= 18) {
+        img.innerHTML = '<img src="tarde.png">'
+        document.body.style.background = '#CCA890'
+        msghr.innerText = `Boa Tarde!`
+    } else {
+        img.innerHTML = '<img src="noite.png">'
+        document.body.style.background = '#636C93'
+        msghr.innerText = `Boa noite!`
+    }
+}
