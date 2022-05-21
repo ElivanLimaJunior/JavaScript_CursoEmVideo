5914578270ef71ea5143996852ddc80bd9272ed6
Elivan-Jr <elivanlimajunior@gmail.com>
2022-05-21 20:27:07 -0300

    Comentando o código

--- aula 12/exercício 2/script.js
+++ aula 12/exercício 2/script.js
@@ -7,8 +7,15 @@ function verificar() {
     if (Fano.value.length == 0 || Number(Fano.value) > ano) { //Number(Fano.value) também poderia ser usado sem o Number para fazer a conversão. funcionaria do mesmo jeito se colocarssemos Fano.value.
         alert('Erro! Verifique os dados e tente novamente!')
     } else {
-        var fsex = document.getElementsByName('radsex')[0]
-        var idade = ano - Number(Fano.value)
-        res.innerHTML = `Idade calculada: ${idade}` //Essa linha é apenas para testar.
+        var fsex = document.getElementsByName('radsex') //ele irá receber o valor marcado no potão.
+        var idade = ano - Number(Fano.value) // calcular o ano - o ano do input para dar a idade. (Number() para converter o valor para numero).
+        var genero = '' // inicialmente o genero não está recebendo valor, para na nossa condição darmos um valor a ele.
+        if (fsex[0].checked) { // [0] é o primeiro fsex o ".checked" é se marcamos essa opção, então o botão se torna checked.
+            genero = 'Homem' // aqui damos a variavel Genero um valor.
+        } else if (fsex[1].checked) { //[1] segundo fsex.
+            genero = mulher
+        }
+        res.style.textAlign = 'center' // estilizando o res.
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
     }
 }
