6f959fda65a42b80813f814b8f19ae8dd8d7010d
Elivan-Jr <elivanlimajunior@gmail.com>
2022-05-23 20:09:32 -0300

    Laços de Repetição

---
+++ aula 13/laços.js
@@ -0,0 +1,19 @@
+var c = 1
+
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <=6)
+
+// No do o teste lógico é feito no final do código.
+
+
+/*
+WHILE é a + usada pois o teste de lógica é feito no começo do código.
+
+var contador = 1
+while (contador <= 6) {
+    console.log(`passo ${contador}`)
+    contador++ // contador = contador + 1
+}
+*/
