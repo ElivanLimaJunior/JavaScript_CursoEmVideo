76042164c79169406a9c5481770f1b15926e8af4
Elivan-Jr <elivanlimajunior@gmail.com>
2022-05-26 19:16:10 -0300

    Laços

---
+++ aula 13/laços2.js
@@ -0,0 +1,26 @@
+// vimos primeiro a repetição usando while e o do
+// vamos ver agora usando o for
+
+
+// como exemplo vamos por primeiro um while
+
+/*
+var c = 1
+while (c<=10) {
+    console.log(`${c}`)
+    c++
+}
+*/
+
+//agora vamos usar o for para fazer a mesma coisa que o while está fazendo
+
+for (var c = 1; c <= 10; c++) {
+    console.log(`${c}`)
+}
+
+/* Distrinchando. 
+
+for (inicio;teste;incremento)
+
+
+*/
