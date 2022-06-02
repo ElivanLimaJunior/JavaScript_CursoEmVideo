91a8d6748e4fa28556b40212bc8fc5a3881cf2ad
Elivan-Jr <elivanlimajunior@gmail.com>
2022-06-02 20:00:09 -0300

    Função Exercício parte 3
    Adicionando o resto das ações do botão finalizar.

--- aula 16/ex/ex001.js
+++ aula 16/ex/ex001.js
@@ -33,10 +33,39 @@ function adicionar() {
         item.text = `Valor ${num.value} adicionado.` // aqui estamos setando qual o texto que essa tag irá receber.
         lista.appendChild(item) // Aqui estamos adicionando a variavel item na nossa lista. (Dessa maneira temos o retorno visual no nosso site.).          AppendChild = Adicionar filho
 
+        res.innerHTML = '' // Quando eu adicionar um Child, o valor dentro de res deve sumir.
+
     } else {
         window.alert('Valor inválido ou já encontrado na lista.')
     }
     // A condição ser execultada:
     num.value = '' // Nosso num vai ser limpo recebendo um valor vazio
     num.focus() // .focus é o foco do nosso mouse, ou seja. . . ele irá voltar para dentro da caixa para digitarmos outro valor
+}
+
+function finalizar() {
+    if(valores.length == 0) {
+        alert('Adicione valores antes de finalizar')
+
+    } else {
+        let tot = valores.length // tot recebe o tamanho da lista valores
+        let maior = valores[0] // criamos uma variavel que irá receber o maior valor encontrado.
+        let menor = valores[0] // criamos uma variavel que irá receber o menor valor encontrado.
+        let soma = 0
+        let media = 0
+
+        for(let pos in valores) {   // for in: criamos uma variavel que irá percorrer todos os valores dentro dentro do nosso vetor.
+            if(valores[pos] > maior) // aqui fazemos uma analise para achar o maior valor dentro do vetor.
+            maior = valores[pos] //  Ao percorrer ele irá ver se o primeiro valor é maior que o prixmo até o final do vetor. se for ele irá colocar esse valor dentro da nossa variavel maior.
+            if(valores[pos] < menor) { // Essa condição fará o mesmo do de cima, porém para achar o menor valor.
+                menor = valores[pos]
+            }
+        }
+        // Texto que será mostrado na tela.
+
+        res.innerHTML = ''
+        res.innerHTML = `<p>Ato todo, temos ${tot} números cadastrados.</p>` // Aqui temos o texto para mostrar o nosso total de números no vetor valores.
+        res.innerHTML = `<p>O maior valor informado é ${maior}.</p>`
+        res.innerHTML = `<p>O menor valor informado é ${menor}.</p>`
+    }
 }
